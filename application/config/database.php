022f7bd069f0dce5a11be9e23d0067231c08fc4d
Mario sanchez <mariojose1693@gmail.com>
2015-09-02 21:31:34 -0600

    inicamos commit para api

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'us-cdbr-azure-southcentral-e.cloudapp.net',
 	'username' => '',
 	'password' => '',
 	'database' => '',
